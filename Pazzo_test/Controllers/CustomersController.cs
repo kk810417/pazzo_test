1832cb8ac64e446540fa7694b2c6e80437a2d55b
cocochen <kk810417@gmail.com>
2019-05-26 15:30:44 +0800

    add edmx add customersController add customers Index.View

---
+++ Pazzo_test/Controllers/CustomersController.cs
@@ -0,0 +1,21 @@
+﻿using Pazzo_test.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pazzo_test.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var result = db.Customers.ToList();
+            return View(result);
+        }
+    }
+}
